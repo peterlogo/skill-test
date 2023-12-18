2c36edf0df847773b952dc1bb886e4a40dbc825d
Peter Logo <peterlogo77@gmail.com>
2023-12-18 15:00:11 -0500

    Auth fix implemented

--- backend/src/services/emailer.service.js
+++ backend/src/services/emailer.service.js
@@ -1,37 +1,43 @@
-const nodemailer = require('nodemailer');
-const i18n = require('i18n')
+const nodemailer = require("nodemailer");
+const i18n = require("i18n");
 i18n.configure({
-    locales: ['En', 'Mn'],
-    directory: __dirname + '/locales',
-    defaultLocale: 'En',
-})
+  locales: ["En", "Mn"],
+  directory: __dirname + "/locales",
+  defaultLocale: "En",
+});
 
 module.exports = {
-    deliverEmail: function (dest, subject, body) {
-        var transport = nodemailer.createTransport({
-            service: process.env.EMAIL_SERVICE,
-            //host: process.env.EMAIL_HOST,
-            //port: Number(process.env.EMAIL_PORT),
-            auth: {
-                //user: process.env.EMAIL_USER,
-                user: process.env.EMAIL,
-                pass: process.env.EMAIL_PWD
-            }
-        });
-    
-        var mailOptions = {
-            from: process.env.EMAIL,
-            to: dest,
-            subject: subject,
-            text: body
-        };
-    
-        transport.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
-    }   
-}
+  deliverEmail: function (dest, subject, body) {
+    var transport = nodemailer.createTransport({
+      //service: process.env.EMAIL_SERVICE,
+      host: process.env.EMAIL_HOST,
+      port: Number(process.env.EMAIL_PORT),
+      auth: {
+        //user: process.env.EMAIL_USER,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PWD,
+      },
+    });
+
+    var mailOptions = {
+      from: process.env.EMAIL,
+      to: dest,
+      subject: subject,
+      text: body,
+    };
+
+    console.log("Sending email to: " + dest);
+    console.log("Subject: " + subject);
+    console.log("Body: " + body);
+    console.log("Email: " + process.env.EMAIL);
+
+    transport.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log("Error sending email: " + error);
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+  },
+};
